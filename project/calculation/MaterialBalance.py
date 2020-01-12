f9810cf8f629e7d292e5973c655afdd19f1e7fa1
Alexander Kolbeko <sasha-kolbeko@mail.ru>
2020-01-12 22:58:48 +0500

    solved memory error with NumPy arrays

--- project/calculation/MaterialBalance.py
+++ project/calculation/MaterialBalance.py
@@ -1,24 +1,20 @@
-
+import sys
 from project.ObjectModels import Grid
 import numpy as np
 import time
 
-def _material_balance(x, y, a_matrix, b_batrix):
+
+def _material_balance(x, y, a_matrix, b_matrix):
     cell = CellsBox.matrix[y, x]
-    print(type(cell.neighbours))
-    for direction, coords in cell.neighbours.items():
+    for direction, coordinates in cell.neighbours.items():
         for_which = cell.cell_number
-        if coords:
-            by_which = CellsBox.matrix[coords[0], coords[1]].cell_number
+        if coordinates:
+            by_which = CellsBox.matrix[coordinates[0], coordinates[1]].cell_number
             a_matrix[for_which, by_which] = 1  # считаю коэффициент СЮДА НАДО СДЕЛАТЬ ВЫЗОВ ПЕРЕСЧЕТА МАТБАЛАНСОВ
-        else:
-            pass
 
-    print(a_matrix)
-    pass
 
-Nx = 10
-Ny = 10
+Nx = 100
+Ny = 100
 DesignVariant = [(0, 0, "inject"), (Ny-1, Nx-1, "extract")]
 
 
@@ -28,19 +24,24 @@ CellsBox = Grid.Grid(Nx, Ny, DesignVariant)
 
 timeAfter = time.time()
 
+
 calculation_steps = int(input("введите число расчетных месяцев:"))  # расчетный шаг в месяцах
 
+a_matrix_oil = np.zeros((Ny * Nx, Nx * Ny), dtype="float16")  # a_matrix{y,x] где у - номер ячейки, для которой считаем производную, x - по которой
+b_matrix_oil = np.zeros((Ny * Nx), dtype="float16")  # один b для каждой ячейки
+a_matrix_water = np.zeros((Ny * Nx, Nx * Ny), dtype="float16")
+b_matrix_water = np.zeros((Ny * Nx), dtype="float16")
 for step in range(calculation_steps):
-    a_matrix_oil = np.zeros((Ny*Nx, Nx*Ny))  # a_matrix{y,x] где у - номер ячейки, для которой считаем производную, x - по которой
-    b_matrix_oil = np.zeros((Ny*Nx))  # один b для каждой ячейки
-    a_matrix_water = np.zeros((Ny*Nx, Nx*Ny))
-    b_matrix_water = np.zeros((Ny*Nx))
     while True:
-
         for y in range(Ny):
             for x in range(Nx):
                 _material_balance(x, y, a_matrix_oil, b_matrix_oil)
                 "#_material_balance(x, y, a_matrix_water, b_matrix_water)"
+
+        # for i in range(Nx*Ny):
+        #     for j in range(Nx*Ny):
+        #         print(a_matrix_oil[i, j], end=" ")
+        #     print("\n")
 
         pressure_accuracy = False
         if not pressure_accuracy:
@@ -49,7 +50,6 @@ for step in range(calculation_steps):
     pass
 
 "#поля для перетоков у объекта скважина сделать в виде словарей {направление: [массив значений по месяцам]}"
-print(type(calculation_steps))
 print(f'тип хранилища ячеек {type(CellsBox)}')
 print(f'время на инициализацию сетки: {timeAfter-timeBefore} секунд')
 
