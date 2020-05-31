c717bfcadea3eda93d5536cbaa0c6d09611a7160
Alexander Kolbeko <sasha-kolbeko@mail.ru>
2020-05-31 20:46:55 +0500

    npv per production wells plot

--- project/calculation/burning.py
+++ project/calculation/burning.py
@@ -18,7 +18,7 @@ def income_calculation(wells_list, last_mouth, price):
     for well in wells_list:
         if well.destiny == 'extract':
             accumulated_production += well.accumulated_oil_production[last_mouth-1]
-    calc_npv = (accumulated_production * 1000 * 0.85 * price)/(amount * nns_cost)
+    calc_npv = (accumulated_production * 1000 * 0.85 * price)/(amount * nns_cost) + 0.8
     return calc_npv
 
 Best_npv = 0
@@ -47,10 +47,11 @@ wells_amount.append(smallest_amount - 1)
 k = 1
 
 Temperature = Tini
-
+economic_list = []
+result = None
 
 for wells in wells_amount:
-    while Temperature > Tmin:
+    while k < 100:
         extract_amount = 0
         DesignVariant = []
         taken_coords = []
@@ -76,16 +77,23 @@ for wells in wells_amount:
         print(f"добывающих {extract_amount} из {wells}")
         print(DesignVariant)
 
+
         result = MaterialBalance.MB_calculation(Nx, Ny, DesignVariant, months)
         NPV = income_calculation(result.wells_list, months, oil_price)
-
+        prodaction_to_injection = 0
+        for well in result.wells_list:
+            if well.destiny is "extract":
+                prodaction_to_injection += 1
+        prodaction_to_injection = prodaction_to_injection
+        economic_list.append((prodaction_to_injection, NPV))
         if NPV > Best_npv:
             Best_variant = result
-            drawing.draw_graph(Nx, Ny, result, months)
         print(NPV)
 
         Temperature = temperature_k(Tini, k)
         k += 1
 
+drawing.draw_pressure_graph(Nx, Ny, result, months)
+drawing.draw_NPV_grapg(economic_list)
 
 print(f"итераций {k}")
--- project/visualisation/drawing.py
+++ project/visualisation/drawing.py
@@ -4,22 +4,42 @@ import numpy
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.ndimage import zoom
 
-def draw_graph(Nx, Ny, myData, month):
+
+def draw_pressure_graph(Nx, Ny, myData, month):
     zoom_power = 2
     x = numpy.arange(0, Nx, 1)
     y = numpy.arange(0, Ny, 1)
     xgrid, ygrid = numpy.meshgrid(x, y)
-    #xgrid = zoom(xgrid, zoom_power )
-    #ygrid = zoom(ygrid, zoom_power)
+    xgrid = zoom(xgrid, zoom_power )
+    ygrid = zoom(ygrid, zoom_power)
     zgrid = numpy.zeros((Ny, Nx))
     for i in range(Ny):
         for j in range(Nx):
             zgrid[i,j] = myData.matrix[i, j].layer_pressure_fluid[month]
-    #zgrid = zoom(zgrid, zoom_power)
-
+    zgrid = zoom(zgrid, zoom_power)
     fiqure = plot.figure()
     axes = Axes3D(fiqure)
     axes.plot_surface(xgrid, ygrid, zgrid, cmap="summer", antialiased=True)
+    axes.set_xlabel("ось X", color="red")
+    axes.set_ylabel("ось Y", color="red")
+    axes.set_zlabel("Давление, атм.", color="red")
     plot.xlim(0, Nx)
     plot.ylim(0, Ny)
+
+    plot.show()
+
+def draw_NPV_grapg(NPV_list):
+    fiqure = plot.figure()
+    wells_amount = []
+    NPV_per_variant = []
+    axes2 = fiqure.add_subplot()
+    for wells, npv in NPV_list:
+        wells_amount.append(wells)
+        NPV_per_variant.append(npv)
+    wells_amount = numpy.array(wells_amount)
+    NPV_per_variant = numpy.array(NPV_per_variant)
+    axes2.scatter(wells_amount, NPV_per_variant)
+    axes2.set_xlabel("Добывающих скважин", color="red")
+    axes2.set_ylabel("NPV", color="red")
+
     plot.show()
